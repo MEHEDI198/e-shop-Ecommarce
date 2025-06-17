30b2afff158ea1b2874fa630ebd3505a02ddc0e6
MEHEDI198 <114200116+MEHEDI198@users.noreply.github.com>
2025-06-17 23:04:03 +0600

    Create Footer.jsx

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Footertop from './Footertop'
+import Footerbottom from './Footerbottom'
+
+const Footer = () => {
+  return (
+    <div>
+      <Footertop />
+      <Footerbottom />
+    </div>
+  )
+}
+
+export default Footer
