b7093918c62f2c1d693d1bcbdf9efd9dfdba5bf7
MEHEDI198 <114200116+MEHEDI198@users.noreply.github.com>
2025-06-17 23:03:56 +0600

    Update Commonlayout.jsx

--- src/components/commonlayouts/Commonlayout.jsx
+++ src/components/commonlayouts/Commonlayout.jsx
@@ -1,16 +1,14 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom';
-import Topbar from '../header/Topbar';
-import Middlebar from '../header/Middlebar';
-import Bottombar from '../header/Bottombar';
 import Header from '../header/Header';
+import Footer from '../footer/Footer';
 
 const Commonlayout = () => {
   return (
     <>  
       <Header /> 
       <Outlet />
-      <div>Footer</div>
+      <Footer />
       
     </>
   ) 
