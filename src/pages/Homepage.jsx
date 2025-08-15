c17286910f0957752f958a7ca653dfe3a6b3bb57
MEHEDI198 <114200116+MEHEDI198@users.noreply.github.com>
2025-08-15 15:12:37 +0600

    Update Homepage.jsx

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Banner from '../components/Banner'
 import Facility from '../components/Facility'
+import Featuredproduct from '../components/Featuredproduct'
 
 const Homepage = () => {
   return (
     <>
       <Banner />
       <Facility />
+      <Featuredproduct/>
     </>
   )
 }
